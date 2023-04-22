57fe4debee44a5b0250d93344fef60da6ec06d5b
mikebrumfield <mikebrumfield30@gmail.com>
2023-04-22 09:29:00 -0500

    exec scripts

--- DeployResource.sh
+++ DeployResource.sh
@@ -5,6 +5,6 @@ TF_PLAN=apply_plan
 pushd tf-module
 terraform init 
 terraform plan -out $TF_PLAN
-terraform apply_plan $TF_PLAN
+terraform apply $TF_PLAN
 
 popd
--- DestroyResource.sh
+++ DestroyResource.sh
@@ -5,6 +5,6 @@ TF_PLAN=destroy_plan
 pushd tf-module
 terraform init 
 terraform plan -destroy -out $TF_PLAN
-terraform apply_plan $TF_PLAN
+terraform apply $TF_PLAN
 
 popd
